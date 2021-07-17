113bacb6b04a083a84275ec48d8a553ece218cfd
fanli <15504610056@163.com>
2021-07-17 23:59:23 +0800

    完善treeSwitcher1

--- src/template/TreeSwitcher1.js
+++ src/template/TreeSwitcher1.js
@@ -24,6 +24,9 @@ class TreeSwitcher1 {
 
         this._panel.appendChild(this._panelContent);
         this._container.appendChild(this._panel);
+
+        this.addBasemapEvent();
+        this.addOverlaysEvent();
     }
 
     layersClassify() {
@@ -49,10 +52,11 @@ class TreeSwitcher1 {
 
         if (len > 0) {
             let li = document.createElement('li');
-            li.className = 'basemap-group';
+            li.className = 'ml-basemap-group';
+            li.id = 'ml-basemap-group';
             let label = document.createElement('label');
             label.innerText = 'Base maps';
-            label.className = 'ml-layer-group'
+            label.className = 'ml-basemap-layer-group'
             li.appendChild(label);
 
             this._panelContent.appendChild(li);
@@ -63,8 +67,8 @@ class TreeSwitcher1 {
             for (let i = 0; i < len; i++) {
                 let { id, name, type } = basemaps[i];
                 listr +=
-                    `<li class='ml-layer'>
-                        <input type='radio' name='ml-${type}-map-radio' id='${id}' checked='checked' />
+                    `<li class='ml-basemap-layer'>
+                        <input type='radio' name='ml-${type}map-radio' id='${id}' checked='checked' />
                         <label for='${type}'>${name ? name : id}</label>
                     </li>`;
             }
@@ -77,12 +81,14 @@ class TreeSwitcher1 {
     }
 
     createOverlaysPanel() {
+        this._checked = {};
         let overlays = this._overlays;
         let len = overlays.length;
 
         if (len > 0) {
             let li = document.createElement('li');
-            li.className = 'overlay-group';
+            li.className = 'ml-overlay-group';
+            li.id = 'ml-overlay-group';
 
             let button = document.createElement('button');
             button.addEventListener('click', e => {
@@ -99,12 +105,13 @@ class TreeSwitcher1 {
 
             let input = document.createElement('input');
             input.type = 'checkbox';
-            input.id = 'ml-overlay-input'
+            input.id = 'ml-layergroup-checkbox';
+            input.checked = 'checked';
 
             let label = document.createElement('label');
             label.innerText = 'Overlays';
-            label.className = 'ml-layer-group'
-            label.for = 'ml-overlay-input';
+            label.className = 'ml-overlay-group-label'
+            label.for = 'ml-layergroup-checkbox';
 
             li.appendChild(button);
             li.appendChild(input);
@@ -126,6 +133,7 @@ class TreeSwitcher1 {
             let { id, name, children } = overlay;
             if (children) {
                 let li = document.createElement('li');
+                li.className = 'ml-overlay-group';
 
                 let button = document.createElement('button');
                 button.addEventListener('click', e => {
@@ -141,9 +149,11 @@ class TreeSwitcher1 {
 
                 let input = document.createElement('input');
                 input.type = 'checkbox';
+                input.name = 'ml-layergroup-checkbox';
+                input.checked = 'checked';
 
                 let label = document.createElement('label');
-                label.className = 'ml-layer-group';
+                label.className = 'ml-overlay-group-label';
                 label.innerText = name ? name : id;
 
                 li.appendChild(button);
@@ -154,10 +164,14 @@ class TreeSwitcher1 {
                 this.createSubTreeNode(children, li);
             } else {
                 let li = document.createElement('li');
-                li.className = 'ml-layer';
+                li.className = 'ml-overlay-layer';
+                li.id = id;
 
                 let input = document.createElement('input');
                 input.type = 'checkbox';
+                input.name = 'ml-overlay-checkbox'
+                input.checked = 'checked';
+                this._checked[id] = true;
 
                 let label = document.createElement('label');
                 label.innerText = name ? name : id;
@@ -172,13 +186,90 @@ class TreeSwitcher1 {
     }
 
     addBasemapEvent() {
+        this._map.on('load', () => {
+            let basemapgroup = document.getElementById('ml-basemap-group');
+            basemapgroup.addEventListener('click', e => {
+                if (e.target.name === 'ml-basemap-radio') {
+                    this.changeBaseMap(e);
+                }
+            });
+        })
     }
 
     addOverlaysEvent() {
+        this._map.on('load', () => {
+            //overlay layer li
+            let liarr = document.getElementsByClassName('ml-overlay-layer');
+            for (let i = 0; i < liarr.length; i++) {
+                let li = liarr[i];
+                let layerId = li.id;
+                li.addEventListener('click', e => {
+                    if (e.target.type === 'checkbox') {
+                        let checked = e.target.checked;
+                        let visibility = checked ? 'visible' : 'none';
+                        this._map.setLayoutProperty(layerId, 'visibility', visibility);
+                        this._checked[layerId] = checked;
 
+                        let parentul = e.target.parentNode.parentNode;
+                        let inputlist = parentul.querySelectorAll('input');
+                        let childCheckCount = 0;
+                        for (let i = 0; i < inputlist.length; i++) {
+                            if (inputlist[i].checked) {
+                                childCheckCount++;
+                            }
+                        }
+                        if (childCheckCount === 0) {
+                            parentul.parentNode.children[1].checked = false;
+                        } else if (childCheckCount === inputlist.length) {
+                            parentul.parentNode.children[1].checked = true;
+                        } else {
+                            console.log(2);
+                        }
+                    }
+                })
+            }
+
+            // overlay layergroup li
+            let liarr2 = document.getElementsByClassName('ml-overlay-group');
+            for (let i = 0; i < liarr2.length; i++) {
+                let li = liarr2[i];
+                let inputList = li.querySelectorAll('input');
+                let rootinput = inputList[0];
+                rootinput.addEventListener('change', e => {
+                    for (let j = 1; j < inputList.length; j++) {
+                        let input = inputList[j];
+                        let checked = rootinput.checked;
+                        input.checked = checked;
+                        if (input.name === 'ml-overlay-checkbox') {
+                            let visibility = checked ? 'visible' : 'none';
+                            let layerId = input.parentNode.id;
+                            this._map.setLayoutProperty(layerId, 'visibility', visibility);
+                            this._checked[layerId] = checked;
+                        }
+                    }
+                })
+            }
+        })
     }
 
     changeBaseMap(e) {
+        let mapstyle = e.target.id;
+        this._map.setStyle(mapstyle);
+
+        this._map.on('style.load', () => {
+            for (let layerId in this._checked) {
+                let visibility = this._checked[layerId] ? 'visible' : 'none';
+                this._map.setLayoutProperty(layerId, 'visibility', visibility);
+            }
+        })
+    }
+
+    layergroupCheckboxChanged() {
+
+    }
+
+    layerCheckboxChanged() {
+
     }
 }
 
